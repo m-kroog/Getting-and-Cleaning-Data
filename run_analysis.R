1bf4a9def3ade557e6001f2ff2d977e7196fe51b
m-kroog <michael.kroog@gmail.com>
2018-07-05 10:45:05 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,5 +1,7 @@
+library(reshape2)
 library(dplyr)
 
+## read all data
 x_test <- read.table("~/UCI HAR Dataset/test/X_test.txt")
 x_train <- read.table("~/UCI HAR Dataset/train/X_train.txt")
 features <- read.table("~/UCI HAR Dataset/features.txt")
@@ -7,52 +9,33 @@ subject_test <- read.table("~/UCI HAR Dataset/test/subject_test.txt")
 subject_train <- read.table("~/UCI HAR Dataset/train/subject_train.txt")
 y_test <- read.table("~/UCI HAR Dataset/test/y_test.txt")
 y_train <- read.table("~/UCI HAR Dataset/train/y_train.txt")
+act_label <- read.table("~/UCI HAR Dataset/activity_labels.txt")
 
-subject <- rbind(subject_test, subject_train)
-colnames(subject) <- c("subjectId")       
- 
+## tidy feature names
 features_2 <- make.names(as.vector(unlist(features[[2]])), unique = TRUE)
 features_2 <- gsub("\\.", "", features_2)
+features_2 <- gsub("mean", "Mean", features_2)
+features_2 <- gsub("std", "Std", features_2)
 
+## combine data sets
+subject <- rbind(subject_test, subject_train)
+colnames(subject) <- c("subjectId")   
 x_test_train <- rbind(x_test, x_train)
 colnames(x_test_train) <- features_2
 
-x_test_train_select <- select(x_test_train, matches("[Mm]ean|[Ss]td"))
+## subset only Mean and Std data
+x_test_train_select <- select(x_test_train, matches("Mean|Std"))
 
+## change y data into a vector
 y_test_train <- as.vector(unlist(rbind(y_test, y_train)))
 
-activityType <- gsub(1, "WALKING", y_test_train)
-activityType <- gsub(2, "WALKING_UPSTAIRS", activityType)
-activityType <- gsub(3, "WALKING_DOWNSTAIRS", activityType)
-activityType <- gsub(4, "SITTING", activityType)
-activityType <- gsub(5, "STANDING", activityType)
-activityType <- gsub(6, "LAYING", activityType)
+## assign activity levels and labels to y data and combine with x data
+activityType <- factor(y_test_train, levels = act_label[,1], labels = act_label[,2])
+xy_test_train <- cbind(subject, activityType, x_test_train_select)
 
-xy_test_train <- cbind(x_test_train_select, activityType, subject)
+## change data frame format and calculate mean
+xy_test_train <- melt(xy_test_train, id = c("subjectId", "activityType"))
+tidydf <- dcast(xy_test_train, subjectId + activityType ~ variable, mean)
 
-walking <- filter(xy_test_train, activityType == "WALKING") %>% select(-activityType)
-walking_up <- filter(xy_test_train, activityType == "WALKING_UPSTAIRS") %>% select(-activityType)
-walking_down <- filter(xy_test_train, activityType == "WALKING_DOWNSTAIRS") %>% select(-activityType)
-sitting <- filter(xy_test_train, activityType == "SITTING") %>% select(-activityType)
-standing <- filter(xy_test_train, activityType == "STANDING") %>% select(-activityType)
-laying <- filter(xy_test_train, activityType == "LAYING") %>% select(-activityType)
-
-walkingMeans <- walking %>% group_by(subjectId) %>% summarize_each(funs(mean)) %>% select(-subjectId)
-walkingUpMeans <- walking_up %>% group_by(subjectId) %>% summarize_each(funs(mean)) %>% select(-subjectId)
-walkingDownMeans <- walking_down %>% group_by(subjectId) %>% summarize_each(funs(mean)) %>% select(-subjectId)
-sittingMeans <- sitting %>% group_by(subjectId) %>% summarize_each(funs(mean)) %>% select(-subjectId)
-standingMeans <- standing %>% group_by(subjectId) %>% summarize_each(funs(mean)) %>% select(-subjectId)
-layingMeans <- laying %>% group_by(subjectId) %>% summarize_each(funs(mean)) %>% select(-subjectId)
-
-subject_id_rep <- rep(seq.int(1:30), times = 6)
-tidydf <- rbind(walkingMeans, walkingUpMeans, walkingDownMeans, sittingMeans, standingMeans, layingMeans)
-
-activityType_rep <- as.data.frame(c(rep("WALKING", times = 30), rep("WALKING_UPSTAIRS", times = 30),
-                      rep("WALKING_DOWNSTAIRS", times = 30), rep("SITING", times = 30),
-                      rep("STANDING", times = 30), rep("LAYING", times = 30)))
-
-colnames(activityType_rep) <- "activityType"
-
-tidydf <- cbind(subject_id_rep, activityType_rep, tidydf)
-
+## write tidy data frame to a txt file
 write.table(tidydf, "C:/Users/miguelito/Documents/week4assign.txt", row.names = FALSE)
